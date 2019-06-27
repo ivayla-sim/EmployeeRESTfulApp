b0af6d4df090b989f4b3ac5fbad82a4d96ad7cfb
Ivayla Simova <ivayla.simova@estafet.com>
2019-06-27 11:26:19 +0300

    Add application.prop env-based files and add profiles in pom file

--- src/main/java/employee/rest/pck/EmployeeApplication.java
+++ src/main/java/employee/rest/pck/EmployeeApplication.java
@@ -1,6 +1,7 @@
 package employee.rest.pck;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class EmployeeApplication {
+
 	
 	public static void main(String[] args ) {
 		SpringApplication.run(EmployeeApplication.class, args);
@@ -17,6 +19,9 @@ public class EmployeeApplication {
 
 @Component
 class DemoCommandLineRunner implements CommandLineRunner {
+	
+	@Value("${spring.application.name}")
+	private String springApplicationName;
 	
 	@Autowired
 	private EmployeeRepository employeeRepository;
@@ -30,10 +35,13 @@ class DemoCommandLineRunner implements CommandLineRunner {
 		emp1.setFirstName("Ivayla");
 		emp1.setLastName("Simova");
 		emp1.setDateOfBirth("31/03/1988");
-		emp1.setAddresses( new String[]{"Bulgaria, Sofia", "Bulgaria, Gradec"} ); 
+		emp1.setAddresses( new String[]{"Bulgaria, Sofia", "Bulgaria, Gradets"} ); 
 		//emp1.setAddresses("Sofia"); 
 		System.out.println (emp1);
 		employeeRepository.save(emp1);
+		
+		System.out.println (springApplicationName);
+		
 	}
 	
 	
--- src/main/java/employee/rest/pck/EmployeeController.java
+++ src/main/java/employee/rest/pck/EmployeeController.java
@@ -3,6 +3,7 @@ package employee.rest.pck;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 //import org.springframework.http.MediaType;
 //import org.springframework.http.ResponseEntity;
